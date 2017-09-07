80d47cb56266ec4f169180786e4e86675dad6f47
jairofg12 <jairofg12@outlook.com>
2017-09-07 13:42:39 -0500

    add with diferent arguments

---
+++ UnitTestProject1/CalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Software.SuperCalculator;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(2,2);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void AddWithDiferentArguments()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(2,5);
+            Assert.AreEqual(7, result);
+        }
+    }
+}
